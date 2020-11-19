0e7b1c9f8c4d5d7ef8e99fa6733db00f1ca59694
xxiong93 <xengxiong93@gmail.com>
2020-11-18 23:21:02 -0600

    Added in a blank table with an externally linked stylesheet for for the table

--- MusicListCode.js
+++ MusicListCode.js
@@ -4,58 +4,69 @@ $(document).ready(getMusic)
 
 function getMusic() {
 
-
-    var songName;
-
     $.ajax({
         type: "get",
         url: "http://www.devcodecampmusiclibrary.com/api/music",
-        success: function(songList) {
-        //    $.find(songList.title);
-            
-            showMusic(songList);
-            showAlbum(songList);
-        }
+        success: function (songList) {
 
+            callAllFunctions(songList);
+            //call the function, append item, then run through loop again
+        }
+            //create a new row for each artist,title,album,genre, and release date added
     });
 };
 
 function showMusic(songs) {
-    console.log(songs);
-    console.log(songs[8].title);
-    for(let i = 0; i < songs.length; i++){
-        document.write(songs[i].title);
+    let songPick;
+    for (let i = 0; i < songs.length; i++) {
+        songPick = songs[i].title;
+       // document.write(songPick);
     }
-    $("#test").html("<div>i</div>")
+    
+    // $("#test").html("<div>songs[i].title</div>")
 }
 
 function showAlbum(albums) {
+    let albumPick;
+    for (let i = 0; i < albums.length; i++) {
+        albumPick = albums[i].album;
+    }
+    // return(albumPick);
+}
 
-    for(let i = 0; i < albums.length; i++){
-        document.write(albums[i].album);
+function showArtist(artists) {
+    let artistPick;
+    for (let i = 0; i < artists.length; i++) {
+        artistPick = artists[i].artist;
         
     }
+    // return(artistPick);
 }
 
-function showArtistI(artists){
-
-    for(let i = 0; i < artist.length; i++){
-        document.write(artists[i].artist);
+function showGenre(genreList) {
+    let genrePick;
+    for (let i = 0; i < genreList.length; i++) {
+        genrePick = genreList[i].genre;
+       
     }
+    // return(genrePick);
 }
 
-function showGenre(genreList){
-
-    for(let i = 0; i < genreList.length; i++){
-        document.write(genreList[i].genre);
+function showDate(date) {
+    let datePick;
+    for (let i = 0; i < date.length; i++) {
+        datePick = date[i].releaseDate;
     }
+    // return(datePick);
 }
 
-function showDate(date){
+function callAllFunctions(songList){
+    showMusic(songList);
+    showAlbum(songList);
+    showArtist(songList);
+    showGenre(songList);
+    showDate(songList);
 
-    for(let i = 0; i < date.length; i++){
-        document.write(date[i].releaseDate);
-    }
 }
 //loop, jQuery.append, .replace, "<<< use these tools"
 //when building table, include a table head with table headers and rows, and then a table body(where id goes)
